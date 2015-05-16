bac2e04b3931668cee349f73602c45a4c5708fe4
jkpark3070 <jkpark3070@gmail.com>
2015-05-16 17:18:57 +0900

    add folder sql ..

---
+++ jee/WebContent/sql/member.sql
@@ -0,0 +1,16 @@
+create table Member(
+userid varchar2(10) ,
+password varchar2(10),
+name varchar2(10) ,
+age number(3),
+salary number(4),
+
+);
+select * from member;
+drop table member
+insert into member values(
+1000,'start',100
+)
+drop table member;
+
+
